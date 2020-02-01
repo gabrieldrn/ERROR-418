fdfae67bfdea951b55f84bf1b119d1a6013d38e9
Grégory Weiss <weiss.gregory48@gmail.com>
2020-02-01 00:14:32 -0500

    Choosing a random server in random rack and setting its color to red

---
+++ Assets/Scripts/Servers/SelectRandomRack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectRandomRack : MonoBehaviour
+{
+    [Header("Number of rack in this bundle : ")]
+    public int NB_RACKS_IN_BUNDLE = 3;
+    [Header("Time between each bundle selection : ")]
+    public float TIME_BEFORE_NEW_SELECTION = 15.0f;
+
+    // Selecting an index
+    public int randomRackIndex;
+
+    float timeLeft;
+
+    void Start()
+    {
+        timeLeft = TIME_BEFORE_NEW_SELECTION;
+    }
+
+    void Update()
+    {
+        // Decreasing time left
+        timeLeft -= Time.deltaTime;
+
+        // Generating a random number if time left isn't 0
+        if (timeLeft < 0)
+        {
+            randomRackIndex = selectedRackIndex();
+
+            // Resetting time left
+            timeLeft = TIME_BEFORE_NEW_SELECTION;
+        }
+    }
+
+    /*
+     * Output : return a random rack index
+     */
+    int selectedRackIndex()
+    {            
+        // Generating a random index
+        randomRackIndex = Random.Range(0, NB_RACKS_IN_BUNDLE);
+        return randomRackIndex;
+    }
+}
---
+++ Assets/Scripts/Servers/SelectRandomServer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectRandomServer : MonoBehaviour
+{
+    [Header("Number of servers in this rack : ")]
+    public int NB_SERVERS_IN_RACK = 5;
+    [Header("Time between each hack : ")]
+    public float TIME_BEFORE_NEW_HACK = 15.0f;
+
+    float timeLeft;
+    int randomServerIndex;
+    GameObject selectedServer;
+    SelectRandomRack parentScript;
+
+    void Start()
+    {
+        // Get selecting rack script
+        parentScript = transform.parent.GetComponent<SelectRandomRack>();
+        timeLeft = TIME_BEFORE_NEW_HACK;
+    }
+
+    void Update()
+    {
+        if (this.isSelected())
+        {
+            // Decreasing time left
+            timeLeft -= Time.deltaTime;
+
+            // Generating a random number if time left isn't 0
+            if (timeLeft < 0)
+            {
+                selectedServer = selectingRandomServer();
+                selectedServer.GetComponent<Renderer>().material.color = new Color(255f, 0f, 0f, 1f);
+
+                // Resetting time left
+                timeLeft = TIME_BEFORE_NEW_HACK;
+            }
+        }
+    }
+
+    /* Selecting a random server in the rack
+     * Output : return chosen server
+     * Additional infos : 
+     *  - Range of random number for selection in (0, NB_SERVERS_IN_RACK)
+     */
+    GameObject selectingRandomServer()
+    {
+        // Generating a random index
+        randomServerIndex = Random.Range(0, NB_SERVERS_IN_RACK);
+
+        // Actually returning the GameObject
+        return this.transform.GetChild(randomServerIndex).gameObject;
+    }
+
+
+    /* If selected index in parent equal to this child index return true
+     *
+     */
+    public bool isSelected()
+    {
+        if (parentScript.randomRackIndex == transform.GetSiblingIndex())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
