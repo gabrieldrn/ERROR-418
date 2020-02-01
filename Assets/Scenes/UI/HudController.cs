09cada8ecde617bcc570cecb79ac83e95e987ffc
Gabriel Derrien <gabriel.derrien@desjardins.com>
2020-02-01 13:22:02 -0500

    Created GameOver UI

--- Assets/Scenes/UI/HudController.cs
+++ Assets/Scenes/UI/HudController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using AmazingUI;
 
 public class HudController : MonoBehaviour
 {
@@ -11,16 +12,65 @@ public class HudController : MonoBehaviour
     public GameObject[] ServersFixedToggles;
     public GameObject[] ServersDownToggles;
 
+    public GameObject HudLayout;
+    public GameObject GameOverLayout;
+
     public void ProgressServersFixed()
     {
-        if (progressFix >= ServersFixedToggles.Length) return;
-        ServersFixedToggles[progressFix++].GetComponent<Toggle>().isOn = true;
+        if (progressFix < ServersFixedToggles.Length)
+        {
+            ServersFixedToggles[progressFix++].GetComponent<Toggle>().isOn = true;
+        }
+        else
+        {
+            //TODO
+        }
     }
 
     public void ProgressServersDown()
     {
-        if (progressDown >= ServersDownToggles.Length) return;
-        ServersDownToggles[progressDown++].GetComponent<Toggle>().isOn = true;
+        if (progressDown < ServersDownToggles.Length)
+        {
+            ServersDownToggles[progressDown++].GetComponent<Toggle>().isOn = true;
+
+            if (progressDown == ServersDownToggles.Length)
+            {
+                ShowGameOver();
+            }
+        }
+    }
+
+    public void ResetProgress()
+    {
+        foreach(GameObject toggle in ServersFixedToggles)
+        {
+            toggle.GetComponent<Toggle>().isOn = false;
+        }
+
+        foreach (GameObject toggle in ServersDownToggles)
+        {
+            toggle.GetComponent<Toggle>().isOn = false;
+        }
+        progressFix = 0;
+        progressDown = 0;
+    }
+
+    private void ShowGameOver()
+    {
+        GameOverLayout.SetActive(true);
+    }
+
+    private void Start()
+    {
+        if(GameOverLayout == null)
+        {
+            Debug.LogError("GameOverLayout is not set");
+        }
+
+        if (HudLayout == null)
+        {
+            Debug.LogError("HudLayout is not set");
+        }
     }
 
 
@@ -35,6 +85,11 @@ public class HudController : MonoBehaviour
         if (Input.GetKeyDown("p"))
         {
             ProgressServersDown();
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            ShowGameOver();
         }
     }
 }
