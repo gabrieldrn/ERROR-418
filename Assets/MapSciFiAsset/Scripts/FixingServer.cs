6df9093a5045de4e49a1192bff7bc51274c29763
Justin Garcia <aeziak@gmail.com>
2020-02-01 20:18:00 -0500

    Cricle QTE Progress Bar

---
+++ Assets/MapSciFiAsset/Scripts/FixingServer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FixingServer : MonoBehaviour
+{
+    public GameObject loadingBar;
+    private int speed; 
+    private float currentAmount;
+    float timestamp = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+        currentAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)){
+            timestamp = Time.time + 0.2f;
+            currentAmount += speed;
+        }
+        if (Time.time >= timestamp) {
+            currentAmount -= (speed*2) * Time.deltaTime; 
+        }
+        if(currentAmount / 100 != 1){
+            loadingBar.transform.GetComponent<Image>().fillAmount = currentAmount / 100;
+        }
+    }
+}
