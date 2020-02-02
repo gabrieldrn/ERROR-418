3b2b142333e772662480b800d353993739d0a264
Baptiste <batzr84@gmail.com>
2020-02-02 12:35:14 -0600

    dqsdqd$

---
+++ Assets/EndZoneTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndZoneTrigger : MonoBehaviour
+{
+    public string NextLevel;
+    public bool active = false;
+
+    void OnTriggerEnter(Collider autre)
+    {
+        if (active && NextLevel != null)
+        {
+            SceneManager.LoadScene(NextLevel);
+        }
+      
+    }
+}
