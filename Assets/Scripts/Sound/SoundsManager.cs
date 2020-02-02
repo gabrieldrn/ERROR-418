f1174466063b73aeb34cd7883f41300e63328acc
Grégory Weiss <weiss.gregory48@gmail.com>
2020-02-02 11:26:56 -0500

    Added sounds for win and loose

---
+++ Assets/Scripts/Sound/SoundsManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsManager : MonoBehaviour
+{
+    public AudioClip gameTheme;
+    public AudioClip gameWon;
+    public AudioClip gameLost;
+    public bool loopBackgroundMusic;
+
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        playBackgroundTheme();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void playBackgroundTheme()
+    {
+        audio.clip = gameTheme;
+        audio.volume = 0.3f;
+        audio.loop = loopBackgroundMusic;
+        audio.Play();
+    }
+
+    public void playWinTheme()
+    {
+        audio.clip = gameWon;
+        audio.volume = 1;
+        audio.Play();
+    }
+
+    public void playLooseTheme()
+    {
+        audio.clip = gameLost;
+        audio.volume = 1;
+        audio.Play();
+    }
+}
